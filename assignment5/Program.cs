1c6785016a24c5c03a9b83a2438f895b0be33e6f
esmaelehab <63879362+esmaelehab@users.noreply.github.com>
2025-07-22 13:53:48 +0300

    question 2

--- assignment5/Program.cs
+++ assignment5/Program.cs
@@ -10,25 +10,35 @@ namespace assignment5
             //Write a program that prints an identity matrix using for loop, in other
             //words takes a value n from the user and shows the identity table of size n *
             //n.
-            Console.WriteLine("enter the number :");
-            int n = int.Parse(Console.ReadLine());
-            int[,] numbers = new int[n, n];
-            for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine($"row number{i+1} ");
-                for (int j = 0; j < n; )
-                {
-                    Console.WriteLine($"enter row number{i+1} coloumn number {j+1} ");
-                    bool isparsed = int.TryParse(Console.ReadLine(), out numbers[i,j]);
-                    if (isparsed)
-                        j++;
-                }
-            }
-            foreach (int number in numbers)
-            {
-                Console.WriteLine(number);
-            }
+            //Console.WriteLine("enter the number :");
+            //int n = int.Parse(Console.ReadLine());
+            //int[,] numbers = new int[n, n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.WriteLine($"row number{i+1} ");
+            //    for (int j = 0; j < n; )
+            //    {
+            //        Console.WriteLine($"enter row number{i+1} coloumn number {j+1} ");
+            //        bool isparsed = int.TryParse(Console.ReadLine(), out numbers[i,j]);
+            //        if (isparsed)
+            //            j++;
+            //    }
+            //}
+            //foreach (int number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
             #endregion
+            #region q2
+            //Write a program in C# Sharp to find the sum of all elements of the array.
+            int[] numbers = {1,4,6,7};
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            Console.WriteLine($"the sum of all elements of the array is : {sum}");
+                #endregion
         }
     }
 }
