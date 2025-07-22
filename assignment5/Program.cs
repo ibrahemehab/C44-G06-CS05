c21cbd96784d7cd03a9301f6778bba10746e2c40
esmaelehab <63879362+esmaelehab@users.noreply.github.com>
2025-07-22 13:05:26 +0300

    question 1

--- assignment5/Program.cs
+++ assignment5/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace assignment5
+﻿using System.Drawing;
+
+namespace assignment5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region q1
+            //Write a program that prints an identity matrix using for loop, in other
+            //words takes a value n from the user and shows the identity table of size n *
+            //n.
+            Console.WriteLine("enter the number :");
+            int n = int.Parse(Console.ReadLine());
+            int[,] numbers = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"row number{i+1} ");
+                for (int j = 0; j < n; )
+                {
+                    Console.WriteLine($"enter row number{i+1} coloumn number {j+1} ");
+                    bool isparsed = int.TryParse(Console.ReadLine(), out numbers[i,j]);
+                    if (isparsed)
+                        j++;
+                }
+            }
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            #endregion
         }
     }
 }
